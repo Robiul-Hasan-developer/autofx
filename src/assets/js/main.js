a4badfc73e706706a315295f54d5bc88ca512943
Robiul Hasan <robiulhasan9559@gmail.com>
2026-01-01 01:25:51 +0600

    (Section) Counter section done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -393,39 +393,40 @@
     });
     // ============== Wishlist button Js End =======================
 
+   // ========================= Counter Up Js Start ===================
+    const counterUp = window.counterUp.default;
+
+    const callback = (entries) => {
+      entries.forEach((entry) => {
+        const el = entry.target;
+        if (entry.isIntersecting && !el.classList.contains("is-visible")) {
+          counterUp(el, {
+            duration: 3500,
+            delay: 16,
+          });
+          el.classList.add("is-visible");
+        }
+      });
+    };
+    const IO = new IntersectionObserver(callback, { threshold: 1 });
+
+    // Banner statistics Counter
+    const statisticsCounter = document.querySelectorAll(".counter");
+    if (statisticsCounter.length > 0) {
+      statisticsCounter.forEach((counterNumber) => {
+        IO.observe(counterNumber);
+      });
+    }
+
+    // performance Count
+    const performanceCount = document.querySelectorAll(".counter");
+    if (performanceCount.length > 0) {
+      performanceCount.forEach((counterNumber) => {
+        IO.observe(counterNumber);
+      });
+    }
     // ========================= Counter Up Js End ===================
-    // const counterUp = window.counterUp.default;
-
-    // const callback = (entries) => {
-    //   entries.forEach((entry) => {
-    //     const el = entry.target;
-    //     if (entry.isIntersecting && !el.classList.contains("is-visible")) {
-    //       counterUp(el, {
-    //         duration: 1500,
-    //         delay: 16,
-    //       });
-    //       el.classList.add("is-visible");
-    //     }
-    //   });
-    // };
-    // const IO = new IntersectionObserver(callback, { threshold: 1 });
-
-    // // Banner statistics Counter
-    // const statisticsCounter = document.querySelectorAll(".counter");
-    // if (statisticsCounter.length > 0) {
-    //   statisticsCounter.forEach((counterNumber) => {
-    //     IO.observe(counterNumber);
-    //   });
-    // }
-
-    // // performance Count
-    // const performanceCount = document.querySelectorAll(".counter");
-    // if (performanceCount.length > 0) {
-    //   performanceCount.forEach((counterNumber) => {
-    //     IO.observe(counterNumber);
-    //   });
-    // }
-    // ========================= Counter Up Js End ===================
+    
   });
   // ==========================================
   //      End Document Ready function
